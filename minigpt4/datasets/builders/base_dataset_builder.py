f57e59d7b324e7eb7af658ca33ba97fae2af1642
sermomac <sermomac@gmail.com>
2023-06-29 11:27:30 +0530

    Commit

--- minigpt4/datasets/builders/base_dataset_builder.py
+++ minigpt4/datasets/builders/base_dataset_builder.py
@@ -1,11 +1,3 @@
-"""
- This file is from
- Copyright (c) 2022, salesforce.com, inc.
- All rights reserved.
- SPDX-License-Identifier: BSD-3-Clause
- For full license text, see the LICENSE_Lavis file in the repo root or https://opensource.org/licenses/BSD-3-Clause
-"""
-
 import logging
 import os
 import shutil
